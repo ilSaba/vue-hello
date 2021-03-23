1f241daf12a4061f0f19d0c479f5648d2e25368e
Andrea Sabatini <sabatini.andrea@outlook.it>
2021-03-23 22:07:30 +0100

    Esercizio + bonus completato

---
+++ master.js
@@ -0,0 +1,9 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Buenas! Questa è la prima esercitazione su VUE!',
+            image: 'https://www.larcadinoepetshop.it/wp-content/uploads/2019/03/articolo-gatto.jpg'
+        }
+    }
+);
